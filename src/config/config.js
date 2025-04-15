89d7fbaa536b9ff8979821b8d2e0b1184c1ae574
rihanazar1 <rihanazhar5@gmail.com>
2025-04-15 11:32:25 +0530

    Remove config folder from Git tracking

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-const _config = {
-    PORT : process.env.PORT,
-    MONGODB : process.env.MONGODB,
-    JWT_SECRET : process.env.JWT_SECRET
-}
-
-const config = Object.freeze(_config)
-
-module.exports = config 
